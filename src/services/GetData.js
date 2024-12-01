225c8e114814f206e5ded4e1f631462871aa2f0f
Hendry Prasetya <muhammad.prasetya@gopay.co.id>
2024-12-01 19:27:55 +0700

    fix: fix loading ui glitch

--- src/services/GetData.js
+++ src/services/GetData.js
@@ -1,19 +1,19 @@
-export function GetData(queryURL){
-    //Fetch API
-    return new Promise((resolve, reject) =>{
-        fetch(queryURL, {
-            mode: 'cors',
-            method: 'GET',
-            headers: new Headers({
-                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-            })
-        })
-        .then((response) => response.json())
-        .then((responseJson) => {
-            resolve(responseJson);
-        })
-        .catch((error) => {
-            reject(error);
-        });
+export function GetData(queryURL) {
+  //Fetch API
+  return new Promise((resolve, reject) => {
+    fetch(queryURL, {
+      mode: "cors",
+      method: "GET",
+      headers: new Headers({
+        "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+      }),
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        resolve(responseJson);
+      })
+      .catch((error) => {
+        reject(error);
       });
+  });
 }
