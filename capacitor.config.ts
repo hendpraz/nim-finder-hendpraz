366ef10a8a3ef2038f951c70f38cb87d65cd6535
Hendry Prasetya <muhammad.prasetya@gopay.co.id>
2025-03-10 10:40:27 +0700

    feat: add capacitor android app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.hendpraz.nimfinder',
+  appName: 'nim-finder-itb',
+  webDir: 'dist'
+};
+
+export default config;
