2ead01f3d09f613fd1827dc72c78e2d0f6879ca9
Hendry Prasetya <muhammad.prasetya@gopay.co.id>
2025-05-28 12:31:36 +0700

    feat: update description

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,6 +1,6 @@
 export const siteConfig = {
   title: 'NIMFinder.com - Cari Mahasiswa ITB, UI, dan UNPAD',
   description:
-    'NIMFinder.com. Cari Nama/NIM Mahasiswa ITB dan UI di sini. NIM Finder terlengkap dari angkatan 2011 hingga angkatan 2024. Termasuk mahasiswa S1, S2, dan S3.',
+    'NIMFinder.com. Cari Nama/NIM Mahasiswa ITB, UI, dan UNPAD di sini. NIM Finder (NPM Finder) terlengkap dari angkatan 2011 hingga angkatan 2024. Termasuk mahasiswa S1, S2, dan S3. UI NPM Finder, ITB NIM Finder, UNPAD NPM Finder.',
   url: 'https://nimfinder.com',
 };
